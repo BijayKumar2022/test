aebdd6c159f087b2ffa5b66ca7d4f967e755f5d1
BijayKumar2022 <105385148+BijayKumar2022@users.noreply.github.com>
2022-05-11 23:35:00 +0530

    Add files via upload

---
+++ webappapi/Controllers/BooksController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webappapi.Models;
+using webappapi.Repositories;
+using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace webappapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookRepository _bookRepository;
+        public BooksController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<books>> GetBooks()
+        {
+            return await _bookRepository.Get();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<books>> GetBooks(int id)
+        {
+            return await _bookRepository.Get(id);
+        }
+    }
+}
---
+++ webappapi/Models/books.cs
@@ -0,0 +1,10 @@
+﻿namespace webappapi.Models
+{
+    public class books
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public sbyte Author { get; set; }
+        public string Description { get; set; }
+    }
+}
